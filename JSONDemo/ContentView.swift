c2bd99a388f5c79c10ca9d5a7edda79e082c224b
XiaNengQi <920986617@qq.com>
2023-02-17 19:44:20 +0800

    Initial Commit

---
+++ JSONDemo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  JSONDemo
+//
+//  Created by 夏能啟 on 2023/2/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JSONDemo/JSONDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONDemoApp.swift
+//  JSONDemo
+//
+//  Created by 夏能啟 on 2023/2/17.
+//
+
+import SwiftUI
+
+@main
+struct JSONDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
